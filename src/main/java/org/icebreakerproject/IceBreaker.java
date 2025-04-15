e786362950fff248d47001aafb54d3b32787519f
JoshuaFarara <joshua.farara@gmail.com>
2025-04-15 18:51:21 -0400

    Restructuring layout, stub coding constructors, final constants for scene,

--- src/main/java/org/icebreakerproject/IceBreaker.java
+++ src/main/java/org/icebreakerproject/IceBreaker.java
@@ -1,20 +1,26 @@
 package org.icebreakerproject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
-//import javafx.geometry.HPos;
-//import javafx.geometry.Pos;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 //import javafx.scene.control.Button;
 //import javafx.scene.control.Label;
 //import javafx.scene.control.RadioButton;
 //import javafx.scene.control.TextField;
 //import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -25,20 +31,38 @@ import javafx.stage.Stage;
 
 public class IceBreaker extends Application {
 
+//	add window datamembers
+	final int WINDOW_WIDTH = 805;
+	final int WINDOW_HEIGHT = 600;
 //	Add Nodes
 	final Button btnAttendance = new Button("Attendance");
 	final Button btnSlideActivities = new Button("Slide Activities");
 	final Button btnQuiz = new Button("Quiz");
 	final Button btnCodeSubmission = new Button("Code Submission");
 	final Button btnPresentation = new Button("Presentation");
+	final Button btnEC = new Button("EC");
 
-
+	Button[] arrButtons = {btnAttendance, btnSlideActivities, btnQuiz, btnCodeSubmission, btnPresentation};
+	protected ArrayList<Button> listButtons = new ArrayList<>(Arrays.asList(arrButtons));
+	
+	
+//	Create COnstructors to create default IceBreaker Program 
+//	public IceBreaker() {
+//		
+//	}
+//	
+	
+	
+	
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {
+		final DropShadow shadow=new DropShadow();
+		
+		//Adding background image
 		ImageView imageView = new ImageView("src/main/resources/images/sweetHeartCircleFront.jpg");
-		imageView.setFitHeight(300);
-		imageView.setFitWidth(405);
-		imageView.setOpacity(.25);
+		imageView.setFitHeight(WINDOW_HEIGHT);
+		imageView.setFitWidth(WINDOW_WIDTH);
+		imageView.setOpacity(.25); // add opacity to an image
 		
 		//Home/Main Text
 		Text text=new Text();
@@ -48,12 +72,21 @@ public class IceBreaker extends Application {
 	    text.setX(100);
 	    text.setY(100);
 
-		Pane pane = new Pane();
+		Pane pane = new Pane(); // pane is used to show the text on the screen, used before showing gridpane
 		pane.getChildren().add(text);
+		
 		 
+		BorderPane borderPane = new BorderPane(); //adding a border pane to hold the buttons dynamically
+		HBox hBoxTop = new HBox();
+		//Will need to replace GridPane for scalability.
 		GridPane gridPane = new GridPane();
 		
+//		for (int i = 0; i < 5; i++) {
+//			
+//			hBoxTop.getChildren().add(gridPane);
+//		}
 		
+		// gridpane before adding the drop shadows, these buttons are now below.
 //		gridPane.add(imageView,);
 //		pane.getChildren().addAll(btnAttendance, btnSlideActivities, btnQuiz, btnCodeSubmission, btnPresentation);
 //		gridPane.add(btnAttendance, 0, 4);
@@ -62,13 +95,15 @@ public class IceBreaker extends Application {
 //		gridPane.add(btnCodeSubmission, 3, 4);
 //		gridPane.add(btnPresentation, 4, 4);
 		
-		final DropShadow shadow=new DropShadow();
+		
 // attempt at switch implementation()come back to this later if implimentation makes sense 
 		//		switch(btn) {
 //		case btnAttendance:
 //			
 //			break;
 //		}
+		
+		
 //		Event Handlers for drop SHadow upon hover!
 		btnAttendance.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() 
 	    {
@@ -157,18 +192,24 @@ public class IceBreaker extends Application {
 	    	}
 	    });
 		
-		
+		//Better to place buttons with a for loop
+		// Adding buttons to the gridpane layout
 	    gridPane.add(btnAttendance, 0, 4);
 	    gridPane.add(btnSlideActivities, 1, 4);
 		gridPane.add(btnQuiz, 2, 4);
 		gridPane.add(btnCodeSubmission, 3, 4);
 		gridPane.add(btnPresentation, 4, 4);
+		gridPane.add(btnEC, 5, 4);
+		
+//		Creating the border pane layout below.
+//		hBoxTop.add(btnAttendance);
 		
 		
+		// Adding stackpane to hold the background image, pane, and buttons on the gridpane
 		StackPane layout = new StackPane();
 		layout.getChildren().addAll(imageView,pane, gridPane );
 
-		Scene scene = new Scene(layout, 405, 300);
+		Scene scene = new Scene(layout, WINDOW_WIDTH, WINDOW_HEIGHT);
 		primaryStage.setTitle("Ice Breaker Program"); // Set title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
